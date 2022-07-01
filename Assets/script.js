aefaa51fc39536ea0c4bc9f63cb492879a9f06ac
tmerritt56 <104723943+tmerritt56@users.noreply.github.com>
2022-07-01 15:04:55 -0500

    Added text to html, and put in for start quiz btn.

---
+++ Assets/script.js
@@ -0,0 +1,7 @@
+// code quiz
+//  Code Start button 
+//  need to have timer start when user is presented with a question 
+//  another question appears after user answers the previous one. 
+//  if user answers question wrongly time will be subtracted from the clock 
+//  when user answers all questions or time runs out, quiz is over. 
+//  once quiz is over user can save their initials and score.  
